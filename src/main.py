a264bba7dce647c90411bc81364a7f8d2030941c
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2025-02-07 11:52:16 +0100

    Add tasks optimization

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from src.routers import schedule
+from src.routers import schedule, production
 
 app = FastAPI()
 
 app.include_router(schedule.router, prefix="/api", tags=["Scheduling"])
+app.include_router(production.router, prefix="/api", tags=["Production Planning"])
